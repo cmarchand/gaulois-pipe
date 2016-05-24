8e3eac5485996b0fac0a7da8c3073e4047ee6987
Christophe Marchand <cmarchand@oxiane.com>
2016-05-24 15:46:22 +0200

    removed call to SaxonApiException.getLineNumber()

--- src/main/java/fr/efl/chaine/xslt/GauloisPipe.java
+++ src/main/java/fr/efl/chaine/xslt/GauloisPipe.java
@@ -474,7 +474,7 @@ public class GauloisPipe {
                 xslCache.put(__href, xsl);
             } catch(SaxonApiException ex) {
                 LOGGER.error("while compiling "+__href);
-                LOGGER.error("SaxonAPIException: "+href+":"+ex.getLineNumber()+" "+ex.getErrorCode()+":"+ex.getMessage());
+                LOGGER.error("SaxonAPIException: "+href+": ["+ex.getErrorCode()+"]:"+ex.getMessage());
                 if(ex.getCause()!=null) {
                     LOGGER.error(ex.getCause().getMessage());
                 }
