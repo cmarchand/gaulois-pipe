3c0447931a511a93a81fe0b60c1946cb311d0942
Christophe Marchand <christophe@marchand.top>
2018-07-23 14:23:14 +0200

    return to old xerces beta, which runs perfectly for XSD 1.1, instead of xerces 12.0

--- gaulois-pipe/src/main/java/fr/efl/chaine/xslt/config/ConfigUtil.java
+++ gaulois-pipe/src/main/java/fr/efl/chaine/xslt/config/ConfigUtil.java
@@ -15,7 +15,6 @@ import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
